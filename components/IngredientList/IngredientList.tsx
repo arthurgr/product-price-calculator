7c8f6bbb24c214201f37a8cf9444e428e568aea0
Arthur Revelski <arthurrevelski@Arthur-Revelskis-iMac.local>
2023-01-30 18:34:15 -0700

    fixed react able TS errors

--- components/IngredientList/IngredientList.tsx
+++ components/IngredientList/IngredientList.tsx
@@ -1,53 +1,10 @@
 import { useMemo } from "react";
-import { useTable } from "react-table";
+import { Column } from "react-table";
 import { Ingredient } from "./interfaces/Ingredient";
-
-function Table({ columns, data }) {
-  // Use the state and functions returned from useTable to build your UI
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({
-      columns,
-      data,
-    });
-
-  // Render the UI for your table
-  return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup) => {
-          return (
-            <tr key={headerGroup.id} {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th key={column.id} {...column.getHeaderProps()}>
-                  {column.render("Header")}
-                </th>
-              ))}
-            </tr>
-          );
-        })}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <tr key={row.id} {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return (
-                  <td key={cell.id} {...cell.getCellProps()}>
-                    {cell.render("Cell")}
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
-}
+import Table from "../Table/Table";
 
 export default function IngredientList() {
-  const columns = useMemo(
+  const columns = useMemo<Column<Ingredient>[]>(
     () => [
       {
         Header: "Measurement Type",
---
+++ components/Table/Table.tsx
@@ -0,0 +1,53 @@
+import { useTable, Column } from "react-table";
+import { Ingredient } from "../IngredientList/interfaces/Ingredient";
+
+export default function Table({
+  columns,
+  data,
+}: {
+  columns: readonly Column<Ingredient>[];
+  data: Array<Ingredient>;
+}) {
+  // Use the state and functions returned from useTable to build your UI
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data,
+    });
+
+  // Render the UI for your table
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => {
+          return (
+            <tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()} key={column.id}>
+                  {column.render("Header")}
+                </th>
+              ))}
+            </tr>
+          );
+        })}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()} key={row.id}>
+              {row.cells.map((cell) => {
+                console.log(cell);
+                return (
+                  <td {...cell.getCellProps()} key={cell.row.id}>
+                    {cell.render("Cell")}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
