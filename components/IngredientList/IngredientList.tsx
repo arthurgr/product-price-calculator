ae61f6a124cd75fa2e1f6720d448220366d0e6d2
Arthur Revelski <arthurrevelski@Arthur-Revelskis-iMac.local>
2023-01-31 20:43:39 -0700

    removed react table

--- components/IngredientList/IngredientList.tsx
+++ components/IngredientList/IngredientList.tsx
@@ -1,43 +1,14 @@
-import { useMemo } from "react";
-import { Column } from "react-table";
-import { Ingredient } from "./interfaces/Ingredient";
 import Table from "../Table/Table";
 import AddIngredientForm from "./elements/AddIngredientForm";
 import { IngredientListProvider } from "./context/IngredientListContext";
 import { IngredientProvider } from "./context/IngredientContext";
 
 export default function IngredientList() {
-  const columns = useMemo<Column<Ingredient>[]>(
-    () => [
-      {
-        Header: "Ingredient",
-        accessor: "ingredient",
-      },
-      {
-        Header: "Measurement Type",
-        accessor: "measurementType",
-      },
-      {
-        Header: "Purchase Size",
-        accessor: "purchaseSize",
-      },
-      {
-        Header: "Average Cost",
-        accessor: "averageCost",
-      },
-      {
-        Header: "Cost Per Ounce",
-        accessor: "costPerOunce",
-      },
-    ],
-    []
-  );
-
   return (
     <IngredientListProvider>
       <IngredientProvider>
         <AddIngredientForm />
-        <Table columns={columns} data={[]} />
+        <Table />
       </IngredientProvider>
     </IngredientListProvider>
   );
--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -1,54 +1,30 @@
-import { useTable, Column } from "react-table";
-import { Ingredient } from "../IngredientList/interfaces/Ingredient";
-
-export default function Table({
-  columns,
-  data,
-}: {
-  columns: readonly Column<Ingredient>[];
-  data: Array<Ingredient>;
-}) {
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({
-      columns,
-      data,
-    });
-
+import useIngredientListContext from "../IngredientList/context/IngredientListContext";
+export default function Table() {
+  const { ingredientListState } = useIngredientListContext();
   return (
-    <table {...getTableProps()}>
+    <table>
       <thead>
-        {headerGroups.map((headerGroup) => {
-          return (
-            // eslint-disable-next-line react/jsx-key
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                // eslint-disable-next-line react/jsx-key
-                <th
-                  {...column.getHeaderProps()}
-                  scope="col"
-                  className="px-6 py-3"
-                >
-                  {column.render("Header")}
-                </th>
-              ))}
-            </tr>
-          );
-        })}
+        <tr>
+          <th>Ingredient</th>
+          <th>Measurement Type</th>
+          <th>Purchase Size</th>
+          <th>Average Cost</th>
+          <th>Cost Per Ounce</th>
+          <th>Delete</th>
+        </tr>
       </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
+      <tbody>
+        {ingredientListState.map((ingredient) => {
           return (
-            // eslint-disable-next-line react/jsx-key
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return (
-                  // eslint-disable-next-line react/jsx-key
-                  <td {...cell.getCellProps()} className="px-6 py-4">
-                    {cell.render("Cell")}
-                  </td>
-                );
-              })}
+            <tr key={ingredient.ingredient}>
+              <td>{ingredient.ingredient}</td>
+              <td>{ingredient.measurementType}</td>
+              <td>{ingredient.purchaseSize}</td>
+              <td>{ingredient.averageCost}</td>
+              <td>cost per ounce</td>
+              <td>
+                <button>delete</button>
+              </td>
             </tr>
           );
         })}
