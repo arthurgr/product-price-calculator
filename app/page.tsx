bcdaf7aae00fa62087b56d104a8af2e6bc184fc0
Arthur Revelski <arthurrevelski@Arthur-Revelskis-iMac.local>
2023-05-02 17:01:14 -0600

    removed unused variables

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import IngredientList from "components/IngredientList/IngredientList";
 import { headers, cookies } from "next/headers";
 
 export default async function Ingredients() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
   const supabase = createServerComponentSupabaseClient({ headers, cookies });
 
   const { data: ingredients } = await supabase.from("ingredients").select();
